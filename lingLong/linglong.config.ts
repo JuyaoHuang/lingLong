30f31bda128eb612427d138e814b399875f896fa
Juyao Huang <tinghua5226@gamil.com>
2025-10-17 15:04:09 +0800

    refresh private inform

--- lingLong/linglong.config.ts
+++ lingLong/linglong.config.ts
@@ -35,7 +35,7 @@ const LingLongConfig: Configuration = {
     },
   ],
   username: "わかば むつみ",
-  sign: "とある科学のみさか みこと",
+  sign: "私の唯一の命は私にかかっている",///我这仅此一次的人生只取决我
   avatarUrl:"/Atri.png",
   socialLinks: [
     {
